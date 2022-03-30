897989abf1bddc38de52fe1c54dcd70f78536b75
Priyesh Kumar <priyeshjnv183@gmail.com>
2022-03-31 00:29:23 +0530

    Update Missing_Number.cpp

--- Arrays/Missing_Number.cpp
+++ Arrays/Missing_Number.cpp
@@ -10,3 +10,19 @@ class Solution{
         return (s-sum);
     }
 };
+
+// Alternate Solution
+class Solution{
+  public:
+    int MissingNumber(vector<int>& array, int n) {
+        // Your code goes here
+        int a=0,b=0;
+        for(int i=0; i<n-1; i++){
+            a ^= array[i];
+        }
+        for(int i=1; i<=n; i++){
+            b ^= i;
+        }
+        return a^b;
+    }
+};
