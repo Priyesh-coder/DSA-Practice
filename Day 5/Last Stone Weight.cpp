a10531df93e96d4154ec977863637f132a50b81a
Priyesh Kumar <priyeshjnv183@gmail.com>
2022-04-15 22:59:00 +0530

    Create Last Stone Weight.cpp

---
+++ Day 5/Last Stone Weight.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int, vector<int>>q;
+        int x,y;
+        for(auto it:stones)
+            q.push(it);
+        
+        while(q.size() != 1){
+            y = q.top();
+            q.pop();
+            x = q.top();
+            q.pop();
+            q.push(y-x);
+        }
+        return q.top();
+    }
+};
