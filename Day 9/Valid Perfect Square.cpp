bfec80f3e4393acf0bf094ca28b534709f8b3717
Priyesh Kumar <priyeshjnv183@gmail.com>
2022-04-17 20:42:02 +0530

    Create Valid Perfect Square.cpp

---
+++ Day 9/Valid Perfect Square.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long int low=0,high=num,mid;
+        while(low<=high){
+            mid = low + (high-low)/2;
+            if(mid*mid < num)
+                low = mid+1;
+            else if(mid*mid > num)
+                high = mid-1;
+            else
+                return true;
+        }
+        return false;
+    }
+};
