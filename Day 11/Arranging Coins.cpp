f29f53d615fd30dde3e381b525517cc8fefb4ca9
Priyesh Kumar <priyeshjnv183@gmail.com>
2022-04-18 23:50:06 +0530

    Update Arranging Coins.cpp

--- Day 11/Arranging Coins.cpp
+++ Day 11/Arranging Coins.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
+       int i=1,count=0;
+       while(n){
+           n -= i++;
+           if(n>=0)
+               count++;
+           else break;
+       }
+        return count;
+    }
+};
+
+// Alternate Solution....
+class Solution {
+public:
+    int arrangeCoins(int n) {
         long long k = sqrt(2* (long long)n);
         long long sum = k*(k+1)/2;
         if(sum > n) return k-1;
