4c5402f2e6b0c39ed429bad5ceb5fb481f9db435
Priyesh Kumar <priyeshjnv183@gmail.com>
2022-04-19 09:47:53 +0530

    Update Average Salary Excluding the Minimum and Maximum Salary.cpp

--- Day 12/Average Salary Excluding the Minimum and Maximum Salary.cpp
+++ Day 12/Average Salary Excluding the Minimum and Maximum Salary.cpp
@@ -3,6 +3,20 @@ public:
     double average(vector<int>& salary) {
         int n = salary.size();
         double sum = 0;
+        sort(salary.begin(),salary.end());
+        for(int i=1; i<n-1; i++){
+            sum += salary[i];
+        }
+        return sum/(n-2);
+    }
+};
+
+// Alternate....
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        int n = salary.size();
+        double sum = 0;
         int mini = INT_MAX;
         int maxi = INT_MIN;
         
