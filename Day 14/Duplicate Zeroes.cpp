feb8b52aa080541ee3ccf7a9df35fad92e6b9e8b
Priyesh Kumar <priyeshjnv183@gmail.com>
2022-04-19 20:34:28 +0530

    Update Duplicate Zeroes.cpp

--- Day 14/Duplicate Zeroes.cpp
+++ Day 14/Duplicate Zeroes.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+    void duplicateZeros(vector<int>& arr) {
+        vector<int> v;
+        for(int i  =0;i<arr.size();i++){
+            v.push_back(arr[i]);
+            if(arr[i] == 0){
+                v.push_back(0);
+                
+            }
+        }
+        for(int j = 0;j<arr.size();j++){
+            arr[j] = v[j];
+        }
+    }
+};
+
+// Alternate Solution...
+
+class Solution {
+public:
     void insert_zero(vector<int>& arr, int i, int j){
         if(j<arr.size())
             arr[j] = arr[i];
