2354136aafbcc48ba5c7d44b4bcb845adde22e7a
Priyesh Kumar <priyeshjnv183@gmail.com>
2022-04-25 23:35:06 +0530

    Update Reverse Integer.cpp

--- Day 27/Reverse Integer.cpp
+++ Day 27/Reverse Integer.cpp
@@ -27,3 +27,21 @@ public:
         return int(num);
     }
 };
+
+// Method --- 2
+class Solution {
+public:
+    int reverse(int x) {
+        int32_t rev_num = 0;
+        
+        while (x)
+        {
+            if((rev_num>INT_MAX/10 || rev_num<INT_MIN/10))
+                return 0;
+        
+            rev_num =( rev_num*10 + x%10);
+            x = x/10;
+        }
+        return rev_num; 
+    }
+};
