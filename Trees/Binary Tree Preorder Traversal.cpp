8b2661a6de0d9bb7563eae93ad6da2c24e3abb30
Priyesh Kumar <priyeshjnv183@gmail.com>
2022-04-29 23:27:39 +0530

    Update Binary Tree Preorder Traversal.cpp

--- Trees/Binary Tree Preorder Traversal.cpp
+++ Trees/Binary Tree Preorder Traversal.cpp
@@ -1,3 +1,4 @@
+// Recursive.....
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -25,4 +26,26 @@ public:
   preorderTraversal(root->right);
   return v;
  }
+};
+
+
+// Iterative....
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>preorder;
+        if(root==NULL)return preorder;
+        
+        stack<TreeNode*>st;
+        st.push(root);
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+            preorder.push_back(root->val);
+            if(root->right != NULL) st.push(root->right);
+            if(root->left != NULL) st.push(root->left);
+
+        }
+        return preorder;
+    }
 };
