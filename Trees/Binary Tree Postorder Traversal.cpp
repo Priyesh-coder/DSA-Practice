6a122fb6f8824ac07a8aafa735cab8f19cdeee6b
Priyesh Kumar <priyeshjnv183@gmail.com>
2022-04-30 00:41:10 +0530

    Update Binary Tree Postorder Traversal.cpp

--- Trees/Binary Tree Postorder Traversal.cpp
+++ Trees/Binary Tree Postorder Traversal.cpp
@@ -21,3 +21,35 @@ public:
         return v;
     }
 };
+
+// Iterative...
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>postorder;
+        if(root == NULL) return postorder;
+        
+        stack<TreeNode*>st;
+        while(root!=NULL || !st.empty()){
+            if(root != NULL){
+                st.push(root);
+                root = root->left;
+            }else{
+                TreeNode* temp = st.top()->right;
+                if(temp==NULL){
+                    temp = st.top();
+                    st.pop();
+                    postorder.push_back(temp->val);
+                    while(!st.empty() && temp == st.top()->right){
+                        temp = st.top();
+                        st.pop();
+                        postorder.push_back(temp->val);
+                    }
+                }else{
+                        root = temp;
+                    }
+            }
+        }
+        return postorder;
+    }
+};
