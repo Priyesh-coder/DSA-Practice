b5cb79984f8f0df8774ebccd23354981112a7b04
Priyesh Kumar <priyeshjnv183@gmail.com>
2022-05-02 09:03:01 +0530

    Create Ceil from BST.cpp

---
+++ Trees/Ceil from BST.cpp
@@ -0,0 +1,18 @@
+int findCeil(BinaryTreeNode<int> *root, int x){
+    // Write your code here.
+	int ceil = -1;
+	while(root){
+		if(root->data == x){
+			ceil = root->data;
+			return ceil;
+		}
+		if(x > root->data){
+			root = root->right;
+		}
+		else{
+			ceil = root->data;
+			root = root->left;
+		}
+	}
+	return ceil;
+}
