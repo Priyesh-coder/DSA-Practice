fda159f6fdc6c5ccc94e2cccfcb590e295caea02
Priyesh Kumar <priyeshjnv183@gmail.com>
2022-05-06 14:42:48 +0530

    Create Sort an Array.cpp

---
+++ Heap/Sort an Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        vector<int>ans;
+        priority_queue<int,vector<int>,greater<int>>pq;
+        
+        for(auto num:nums)
+            pq.push(num);
+        
+        while(!pq.empty()){
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
+};
